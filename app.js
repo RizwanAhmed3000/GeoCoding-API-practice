23b63646a896b47c6f122023dd6e1b7dfca435dd
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-06-24 12:37:22 +0500

    geocode api fetching

---
+++ app.js
@@ -0,0 +1,50 @@
+const mainContainer = document.querySelector('.mainContainer');
+
+function whaerAmI(lat, lng){
+    const fetchApi = fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    fetchApi
+        .then((response)=> {
+            if(!response.ok){
+                throw new Error(`There is a problem with with your Coordinates, your status code in ${response.status}`)
+            }else{
+                console.log(response)
+                return response.json()
+            }
+        })
+        .then((data)=> {
+            console.log(data);
+            console.log(`You are in ${data.country}`);
+            return fetch(`https://restcountries.com/v3.1/name/${data.country}`)
+        })
+        .then((counrtyResp)=>{
+            console.log(counrtyResp, "==>> country response");
+            if(!counrtyResp.ok){
+                throw new Error(`Country not found, status code ${counrtyResp.status}`)
+            } else{
+                return counrtyResp.json();
+            }
+        })
+        .then((countryData)=>{
+            console.log(countryData[0], "==>> country data");
+            uiCreation(countryData[0]);
+        })
+        .catch((err)=> console.log(err))
+}
+
+function uiCreation(country){
+    const card = `<div class="card" style="width: 18rem;">
+    <img id="image" src=${country.flags.png} class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${country.name.common}</h5>
+        <p class="card-text">${country.name.official}</p>
+        <h5 class="card-title">Currencies:</h5>
+        <p class="card-text">${country.currencies.GBP.name}(${country.currencies.GBP.symbol})</p>
+        <h5 class="card-title">Languages:</h5>
+        <p class="card-text">${country.languages.eng}</p>
+    </div>
+</div>`
+
+    mainContainer.innerHTML = card;
+}
+
+// whaerAmI(52.508, 13.381);
