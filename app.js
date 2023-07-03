747c09a77af919c9d7e301fb2ef84d3e4924ab02
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-07-03 17:21:23 +0500

    async await

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
 const mainContainer = document.querySelector('.mainContainer');
 
-function whaerAmI(lat, lng){
-    const fetchApi = fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    fetchApi
-        .then((response)=> {
-            if(!response.ok){
-                throw new Error(`There is a problem with with your Coordinates, your status code in ${response.status}`)
-            }else{
-                console.log(response)
-                return response.json()
-            }
-        })
-        .then((data)=> {
-            if(data.distance == "Throttled! See geocode.xyz/pricing"){
-                throw new Error('API is throttling')
-            } else{
-                console.log(data);
-                console.log(`You are in ${data.country}`);
-                return fetch(`https://restcountries.com/v3.1/name/${data.country}`)
-            }
-        })
-        .then((counrtyResp)=>{
-            console.log(counrtyResp, "==>> country response");
-            if(!counrtyResp.ok){
-                throw new Error(`Country not found, status code ${counrtyResp.status}`)
-            } else{
-                return counrtyResp.json();
-            }
-        })
-        .then((countryData)=>{
-            console.log(countryData[0], "==>> country data");
-            uiCreation(countryData[0]);
-        })
-        .catch((err)=> {
-            console.log(err);
-            mainContainer.innerHTML = `<h5>${err}</h5>`
-        })
+async function whaerAmI(lat, lng) {
+    try {
+        const fetchApi = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+        if (!fetchApi.ok) {
+            throw new Error(`There is a problem with with your Coordinates, your status code in ${response.status}`)
+        }
+        const data = await fetchApi.json();
+        if (data.distance == "Throttled! See geocode.xyz/pricing") {
+            throw new Error('API is throttling')
+        }
+        console.log(data);
+        console.log(`You are in ${data.country}`);
+        const countryApi = await fetch(`https://restcountries.com/v3.1/name/${data.country}`)
+        if (!countryApi.ok) {
+            throw new Error(`Country not found, status code ${counrtyResp.status}`)
+        }
+        const countryData = await countryApi.json();
+        uiCreation(countryData[0]);
+    } catch (error) {
+        console.error(error);
+        mainContainer.innerHTML = `<h5>${err}</h5>`
+    }
 }
 
-function uiCreation(country){
+function uiCreation(country) {
     const card = `<div class="card" style="width: 18rem;">
     <img id="image" src=${country.flags.png} class="card-img-top" alt="...">
     <div class="card-body">
